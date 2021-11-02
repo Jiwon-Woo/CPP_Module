7cf4b897dbc28389a2a2be55b4f5ec1439a770ae
Jiwon Woo <jwoo@c8r7s2.42seoul.kr>
2021-11-02 17:42:28 +0900

    [21/11/02 17:42] cpp01/ex02 추가

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int	main()
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "   string address: " << &str << std::endl;
+	std::cout << "stringPTR address: " << stringPTR << std::endl;
+	std::cout << "stringREF address: " << &stringREF << std::endl;
+
+	std::cout << "     string value: " << str << std::endl;
+	std::cout << "  stringPTR value: " << *stringPTR << std::endl;
+	std::cout << "  stringREF value: " << stringREF << std::endl;
+
+	return 0;
+}
