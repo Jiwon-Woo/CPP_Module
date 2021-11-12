7e697005622231b92e4413dc397c6cf1ae7f639a
Jiwon Woo <jiwonwoo@MacBookAir.local>
2021-11-13 00:12:35 +0900

    [21/11/13 00:12] cpp00/ex00 to_upper 함수 cctype 헤더 불러와서 사용

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
-#include <cstring>
-
-char	to_upper(char c)
-{
-	if ('a' <= c && c <= 'z')
-		c -= 32;
-	return (c);
-}
+#include <cctype>
 
 int	main(int argc, char **argv)
 {
@@ -14,7 +7,7 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < argc; i++)
 		for (int j = 0; j < (int)strlen(argv[i]); j++)
-			std::cout << to_upper(argv[i][j]);
+			std::cout << (char)toupper(argv[i][j]);
 	std::cout << std::endl;
 	return 0;
 }
