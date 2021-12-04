a2cf163fddb8815d214910a5d7a897cabacd3154
Jiwon Woo <jiwonwoo@MacBookAir.local>
2021-12-04 17:56:43 +0900

    [21/12/04 17:56] DiamondTrap 클래스 구현

--- cpp03/ex03/DiamondTrap.cpp
+++ cpp03/ex03/DiamondTrap.cpp
@@ -1,1 +1,83 @@
 #include "DiamondTrap.hpp"
+
+DiamondTrap::DiamondTrap() : ClapTrap(), FragTrap(), ScavTrap(), name("")
+{
+	ClapTrap::name += "_clap_name";
+	this->hitPoints = FRAG_HIT_POINTS;
+	this->energyPoints = SCAV_ENERGY_POINTS;
+	this->attackDamage = FRAG_ATTACK_DAMAGE;
+	std::cout << "Default DiamondTrap created" << std::endl;
+}
+
+DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name), FragTrap(name), ScavTrap(name), name(name)
+{
+	ClapTrap::name += "_clap_name";
+	this->hitPoints = FRAG_HIT_POINTS;
+	this->energyPoints = SCAV_ENERGY_POINTS;
+	this->attackDamage = FRAG_ATTACK_DAMAGE;
+	std::cout << "DiamondTrap <" << this->name << "> created" << std::endl;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap &diamondTrap)
+{
+	*this = diamondTrap;
+}
+
+DiamondTrap::~DiamondTrap()
+{
+	std::cout << "DiamondTrap <" << this->name << "> destroyed" << std::endl;
+}
+
+DiamondTrap&	DiamondTrap::operator=(const DiamondTrap &diamondTrap)
+{
+	this->name = diamondTrap.getName();
+	ClapTrap::name = diamondTrap.ClapTrap::getName();
+	this->hitPoints = diamondTrap.getHitPoints();
+	this->energyPoints = diamondTrap.getEnergyPoints();
+	this->attackDamage = diamondTrap.getAttackDamage();
+	return (*this);
+}
+
+std::string	DiamondTrap::getName() const
+{
+	return this->name;
+}
+
+void	DiamondTrap::takeDamage(unsigned int amount)
+{
+	if (this->hitPoints <= 0)
+	{
+		std::cout << "DiamondTrap <" << this->name << "> is died" << std::endl;
+		return;
+	}
+	std::cout << "DiamondTrap <" << this->name << "> is attacked. The remaining HP is ";
+	if (this->hitPoints <= amount)
+	{
+		this->hitPoints = 0;
+		std::cout << "0. DiamondTrap <" << this->name << "> is died" << std:: endl;
+	}
+	else
+	{
+		this->hitPoints -= amount;
+		std::cout << this->hitPoints << "." << std::endl;
+	}
+}
+
+void	DiamondTrap::beRepaired(unsigned int amount)
+{
+	if (this->hitPoints <= 0)
+		std::cout << "DiamondTrap <" << this->name << "> is respawned. ";
+	else
+		std::cout << "DiamondTrap <" << this->name << "> is repaired. ";
+	this->hitPoints += amount;
+	std::cout << "The remaining HP is " << this->hitPoints << "." << std::endl;
+}
+
+void	DiamondTrap::whoAmI()
+{
+	std::cout << "##################" << std::endl;
+	std::cout << "#    Who am I    #" << std::endl;
+	std::cout << "##################" << std::endl;
+	std::cout << "diamondTrap name  : " << this->name << std::endl;
+	std::cout << "clapTrap name     : " << ClapTrap::name << std::endl;
+}
--- cpp03/ex03/DiamondTrap.hpp
+++ cpp03/ex03/DiamondTrap.hpp
@@ -1,12 +1,24 @@
 #ifndef DIAMONDTRAP_HPP
-# define DIAMONTRAP_HPP
+# define DIAMONDTRAP_HPP
 
-# include "FragTrap.hpp"
 # include "ScavTrap.hpp"
+# include "FragTrap.hpp"
 
-class DiamondTrap : public FragTrap, ScavTrap
+class DiamondTrap : virtual public FragTrap, virtual public ScavTrap
 {
-
+protected:
+	std::string	name;
+public:
+	DiamondTrap();
+	DiamondTrap(std::string name);
+	DiamondTrap(const DiamondTrap &diamondTrap);
+	~DiamondTrap();
+	DiamondTrap&	operator=(const DiamondTrap &diamondTrap);
+	using	FragTrap::attack;
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+	std::string	getName() const;
+	void	whoAmI();
 };
 
 #endif
