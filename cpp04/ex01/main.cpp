58c9b700774a1b2bc5a4389b5a872ea3f6dc19fd
Jiwon Woo <jwoo@c2r4s5.42seoul.kr>
2021-12-11 17:19:55 +0900

    [21/12/11 17:19] ex01main 문에 Animal 객체 포인터 배열 추가

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -2,35 +2,117 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
+# define ANIMAL_NUM 4
+# define DOG_INDEX 0
+# define CAT_INDEX 1
+
+# define C_NRML "\033[0m"
+# define C_YLLW "\033[33m"
+# define C_RED  "\033[31m"
+
 int main()
 {
-	Dog* j = new Dog();
-	Cat* i = new Cat();
+	{
+		std::cout << std::endl;
+		std::cout << "###############" << std::endl;
+		std::cout << "# check leaks #" << std::endl;
+		std::cout << "###############" << std::endl;
+		std::cout << std::endl;
 
-	// j->getBrain().addIdea("hi");
-	// std::cout << j->getBrain().getIdea()->size() << std::endl;
-	j->addIdea(std::string("hello"));
-	j->addIdea("good");
+		const Animal* j = new Dog();
+		std::cout << std::endl;
 
-	std::cout << std::endl;
-	j->getBrain().showIdea();
-	std::cout << std::endl;
+		const Animal* i = new Cat();
+		std::cout << std::endl;
 
-	Dog z = Dog();
+		delete j;//should not create a leak
+		std::cout << std::endl;
+		delete i;
+		std::cout << std::endl << std::endl;
+	}
 
-	*j = z;
-	Dog y = Dog(z);
+	{
+		std::cout << std::endl;
+		std::cout << "###########################" << std::endl;
+		std::cout << "# UpCasting & DownCasting #" << std::endl;
+		std::cout << "###########################" << std::endl;
+		std::cout << std::endl;
 
-	j->getBrain().showIdea();
-	y.getBrain().showIdea();
+		std::cout << C_RED << "Create and Fill an Array of Animal" << C_NRML << std::endl;
+		Animal*	animals[ANIMAL_NUM];
 
-	z.addIdea("what");
+		for (int i = 0; i < ANIMAL_NUM; i++)
+		{
+			if (i % 2)
+				animals[i] = new Cat();
+			else
+				animals[i] = new Dog();
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
 
-	j->getBrain().showIdea();
-	y.getBrain().showIdea();
-	z.getBrain().showIdea();
+		std::cout << C_YLLW << "((Dog *)animals[DOG_INDEX])->addIdea(2)" << C_NRML << std::endl;
+		((Dog *)animals[DOG_INDEX])->addIdea("hello");
+		((Dog *)animals[DOG_INDEX])->addIdea("good");
+		std::cout << std::endl;
 
-	delete j;//should not create a leak
-	delete i;
+
+		std::cout << C_YLLW << "((Dog *)animals[DOG_INDEX])->showIdea()" << C_NRML << std::endl;
+		((Dog *)animals[DOG_INDEX])->showIdea();
+		std::cout << std::endl;
+
+		std::cout << C_YLLW << "Dog dog = *animals[DOG_INDEX]" << C_NRML << std::endl;
+		Dog dog = *((Dog *)animals[DOG_INDEX]);
+		std::cout << std::endl;
+
+		std::cout << C_YLLW << "Cat cat = Cat(*((Cat *)animals[CAT_INDEX]))" << C_NRML << std::endl;
+		Cat cat = Cat(*((Cat *)animals[CAT_INDEX]));
+		std::cout << std::endl;
+
+		std::cout << C_YLLW << "dog.showIdea()" << C_NRML << std::endl;
+		dog.showIdea();
+		std::cout << std::endl;
+
+		std::cout << C_YLLW << "cat.showIdea()" << C_NRML << std::endl;
+		cat.showIdea();
+		std::cout << std::endl;
+
+		std::cout << C_YLLW << "dog.addIdea(what)" << C_NRML << std::endl;
+		dog.addIdea("what");
+		std::cout << std::endl;
+
+		std::cout << C_YLLW << "((Dog *)animals[DOG_INDEX])->showIdea()" << C_NRML << std::endl;
+		((Dog *)animals[DOG_INDEX])->showIdea();
+		std::cout << std::endl;
+
+		std::cout << C_YLLW << "dog.showIdea()" << C_NRML << std::endl;
+		dog.showIdea();
+		std::cout << std::endl;
+
+		std::cout << C_YLLW << "((Cat *)animals[CAT_INDEX])->addIdea(nothing)" << C_NRML << std::endl;
+		((Cat *)animals[CAT_INDEX])->addIdea("nothing");
+		std::cout << std::endl;
+
+		std::cout << C_YLLW << "cat.addIdea(something)" << C_NRML << std::endl;
+		cat.addIdea("something");
+		std::cout << std::endl;
+
+		std::cout << C_YLLW << "((Cat *)animals[CAT_INDEX])->showIdea()" << C_NRML << std::endl;
+		((Cat *)animals[CAT_INDEX])->showIdea();
+		std::cout << std::endl;
+
+		std::cout << C_YLLW << "cat.showIdea()" << C_NRML << std::endl;
+		cat.showIdea();
+		std::cout << std::endl;
+
+		std::cout << std::endl;
+		std::cout << C_RED << "Destroy an Array of Animal" << C_NRML << std::endl;
+		for (int i = 0; i < ANIMAL_NUM; i++)
+		{
+			delete animals[i];
+			std::cout << std::endl;
+		}
+		std::cout << C_RED << "Destroy Cat and Dog" << C_NRML << std::endl;
+	}
 	while (1);
 }
